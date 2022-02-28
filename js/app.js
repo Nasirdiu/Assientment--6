fab53931dd1d373364ef9efb9b0f7afdb09ccc66
nasirdiu <nasir.joypurhat@gmail.com>
2022-02-28 23:12:02 +0600

    diteles

--- js/app.js
+++ js/app.js
@@ -6,27 +6,50 @@ const phoneSearch = () => {
     .then((res) => res.json())
     .then((data) => displayPhone(data.data));
 };
-phoneSearch();
+// phoneSearch();
 const displayPhone = (search) => {
   // console.log(search);
   const firstCard = document.getElementById("first-card");
   search.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const div = document.createElement("div");
     div.classList.add('col');
     div.innerHTML = `
-        <div class="card">
-          <img class="w-50 mt-3 img-thumbnail rounded mx-auto d-block" src="${phone.image}" class="card-img-top" alt="..." />
+        <div class="card ">
+          <img class="w-50 mt-3 img-thumbnail rounded mx-auto" src="${phone.image}" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Brand:${phone.brand}</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+            <h4 class="card-title">Brand:${phone.brand}</h4>
+            <h5 class="card-text">Phone Name:${phone.phone_name}  </h5>
           </div>
+          <button onclick="displayId('${phone.slug}')" class="btn btn-success w-25 mx-auto 
+          mb-3">Details</button>
+       
       </div>`;
 
     firstCard.appendChild(div);
   });
 };
+
+const displayId=id=>{
+    const url=`https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data =>detailsDisplay(data.data))
+}
+
+const detailsDisplay=(show)=>{
+   console.log(show);
+    const secendCard=document.getElementById('secend-card'); 
+    const div=document.createElement('div');
+    div.innerHTML=`
+    <div class="card">
+          <img class="w-50 mt-3 img-thumbnail rounded mx-auto" src="${show.image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h4 class="card-title">Brand:${show.brand}</h4>
+            <h5 class="card-text">Phone Name:${show.phone_name}  </h5>
+          </div>
+      </div>
+   `
+  secendCard.appendChild(div); 
+    }
+        
