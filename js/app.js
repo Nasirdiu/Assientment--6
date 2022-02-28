1adc19e9f22c5dc14c28c3686c635586c598f850
nasirdiu <nasir.joypurhat@gmail.com>
2022-03-01 02:02:27 +0600

    other add

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ const phoneSearch = () => {
   const searchInput = document.getElementById("Search-input");
   //error
   const error = document.getElementById("error");
-  error.style.display = "none";
+
   const SearchValue = searchInput.value;
   searchInput.value = "";
   if (!isNaN(SearchValue)) {
     error.style.display = "block";
-  } else{
+  } else {
     fetch(
       `https://openapi.programming-hero.com/api/phones?search=${SearchValue}`
     )
@@ -29,7 +29,7 @@ const displayPhone = (search) => {
             <h4 class="card-title">Brand:${phone.brand}</h4>
             <h5 class="card-text">Phone Name:${phone.phone_name}  </h5>
           </div>
-          <button onclick="displayId('${phone.slug}')" class="btn btn-success w-25 mx-auto 
+          <button onclick="displayId('${phone.slug}')" class="btn btn-outline-success w-25 mx-auto 
           mb-3">Details</button>
        
       </div>`;
@@ -37,30 +37,57 @@ const displayPhone = (search) => {
     firstCard.appendChild(div);
   });
 };
-
+//id show
 const displayId = (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => detailsDisplay(data.data));
 };
-
+//Details show
 const detailsDisplay = (show) => {
-  console.log(show);
+  //   console.log(show);
   const secendCard = document.getElementById("secend-card");
   secendCard.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
     <div class="card">
-          <img class="w-50 mt-3 img-thumbnail rounded mx-auto" src="${show.image}" class="card-img-top" alt="..." />
+          <img class="w-50 mt-3 img-thumbnail rounded mx-auto" src="${
+            show.image
+          }" class="card-img-top" alt="..." />
           <div class="card-body">
-          <h5 class="card-text"> ReleaseDate:-${show.releaseDate}  </h5>
-           <p class="card-title">ChipSet:-${show.mainFeatures.chipSet} </p>
-            <p class="card-title">Storage:-${show.mainFeatures.storage} </p>
-            <p class="card-title"> displaySize:-${show.mainFeatures.displaySize}</p>
-            <p class="card-title">Memory:-${show.mainFeatures.memory}  </p>
-            <p class="card-title">Sensors:-${show.mainFeatures.sensors} </p>
-          
+          <h5 class="card-text"> ReleaseDate:-${
+            show.releaseDate ? show.releaseDate : "No Release Date Found"
+          }  </h5><hr>
+           <p class="card-title">ChipSet:-${show.mainFeatures.chipSet} </p><hr>
+            <p class="card-title">Storage:-${show.mainFeatures.storage} </p><hr>
+            <p class="card-title"> displaySize:-${
+              show.mainFeatures.displaySize
+            }</p><hr>
+            <p class="card-title">Memory:-${show.mainFeatures.memory}  </p><hr>
+            <h4>Sensors Option</h4>
+            <p class="card-title">Sensors:-${show.mainFeatures.sensors} </p><hr>
+            <h4>Other Option</h4>
+            <p class="card-title">Others Bluetooth:-${
+              show.others?.Bluetooth
+                ? show.others.Bluetooth
+                : "Coming Soon Data"
+            } </p><hr>
+            <p class="card-title">Others GPS:-${
+              show.others?.GPS ? show.others.GPS : "Coming Soon Data"
+            } </p><hr>
+            <p class="card-title">Others USB:-${
+              show.others?.USB ? show.others.USB : "Coming Soon Data"
+            } </p><hr>
+            <p class="card-title">Others WLAN:-${
+              show.others?.WLAN ? show.others.WLAN : "Coming Soon Data"
+            } </p><hr>
+            <p class="card-title">Others Radio:-${
+              show.others?.Radio ? show.others.Radio : "Coming Soon Data"
+            } </p><hr>
+            <p class="card-title">Others NFC:-${
+              show.others?.NFC ? show.others.NFC : "Coming Soon Data"
+            } </p>
           </div>
       </div>
    `;
